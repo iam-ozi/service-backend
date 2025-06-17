b70376c27765d7c532e04f529928a19550e03c4e
iam-oz <oziomaudah@gmail.com>
2025-06-16 21:08:01 -0500

    updated java with springbolt simple web app

--- java-app/src/main/java/com/iamozi/backend/App.java
+++ java-app/src/main/java/com/iamozi/backend/App.java
@@ -1,13 +1,24 @@
 package com.iamozi.backend;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Java Spring Boot REST API!";
+    }
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
     }
 }
